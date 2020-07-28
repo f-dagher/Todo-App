78ef96a58da5efda835233d947587b28a978083b
f-dagher <fdagher95@gmail.com>
2020-07-28 18:15:19 -0400

    Updates

---
+++ precache-manifest.ae086c3976e3d8e3aa8fe3763f5a59d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea683dd7b82cb35b10defe15382d72c6",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "9b9c17d2c3ead214bf57",
+    "url": "/todo-app/static/css/main.18c150a0.chunk.css"
+  },
+  {
+    "revision": "c5649cfa508b5fc6b990",
+    "url": "/todo-app/static/js/2.7700c087.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.7700c087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9c17d2c3ead214bf57",
+    "url": "/todo-app/static/js/main.20786b49.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
